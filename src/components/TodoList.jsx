cd031d8fe3f84455906c0447cd0627c0de2b1ac3
Fymos <zufarovfayzullo703@gmail.com>
2024-04-01 13:22:21 +0500

    commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import icon from "../assets/img/trash3.svg";
+const TodoList = () => {
+  return (
+    <div className="wrapper">
+      <h2>Todo App</h2>
+      <div className="todo__add">
+        <input type="text" placeholder="Add your new todo" />
+        <button className="add__btn">+</button>
+      </div>
+      <ul className="todo__list">
+        <li className="todo__item">
+          <p className="todo__text">Lorem ipsum dolor sit amet.</p>
+          <button className="todo__del">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash3"
+              viewBox="0 0 16 16"
+            >
+              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+            </svg>
+          </button>
+        </li>
+      </ul>
+      <div className="todo__footer">
+        <h4 className="total__number">You have 3 pending tasks</h4>
+        <button>Clear All</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
